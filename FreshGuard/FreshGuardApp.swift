d62d2594dc196850c129d0af6fa679bfbe7dde8d
tatjanpozalija3-cpu <tatjanpozalija3@icloud.com>
2025-11-10 10:16:35 +0100

    Add files via upload

---
+++ FreshGuard/FreshGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshGuardApp.swift
+//  FreshGuard
+//
+//  Created by Charlie Hebdo on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct FreshGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
